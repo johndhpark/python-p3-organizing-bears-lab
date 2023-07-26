68c310850537c632b2a5ad5b425eac7e503b6bde
John DH. Park <john.dh.park@icloud.com>
2023-07-26 14:46:16 -0700

    Additional Changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 1, "M", "Black", "Angry", TRUE),
+("Rowdy", 2, "F", "Blue", "Aggresive", TRUE),
+("Tabitha", 3, "M", "Green", "Intense", TRUE),
+("Sergeant Brown", 4, "F", "Purple", "Naive", FALSE),
+("Melissa", 5, "M", "Brown", "Kind", FALSE),
+("Grinch", 7, "M", "Green", "Grinchy", TRUE),
+("Wendy", 3, "F", "Pink", "Happy", TRUE),
+(NULL, 1, "F", "White", "Shy", TRUE);
